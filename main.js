3b5969f95204a085eb81e22b59b13cffcecf3d36
青木秀仁 <shamrock1976@me.com>
2018-04-07 23:13:42 +0900

    ファーストコミット

---
+++ main.js
@@ -0,0 +1,39 @@
+var map;
+var info ;
+
+function main() {
+	var latlng = [35.737841　, 139.653912];
+
+	map = L.map('map').setView(latlng, 15);
+	
+	var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+	
+	L.tileLayer(
+		'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		{
+			attribution: 'Map data &copy; ' + mapLink,
+			maxZoom: 18
+		}
+	).addTo(map);
+	
+	onCreate() ;
+		
+	map.on('click', onMapClick);
+	
+	/*
+	map.locate({setView: true, maxZoom: 16});
+	
+	map.on('locationfound', function(e) {
+		var radius = e.accuracy / 2;
+	
+		L.marker(e.latlng).addTo(map)
+			.bindPopup("You are within " + radius + " meters from this point").openPopup();
+	
+		L.circle(e.latlng, radius).addTo(map);
+	});
+	
+	map.on('locationerror', function(e) {
+	
+	});
+	*/
+}
